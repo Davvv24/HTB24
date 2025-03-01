749a4fc28704e4b4563936e17b7836fbd87f46e2
alexsykes8 <65182304+alexsykes8@users.noreply.github.com>
2025-03-01 16:57:35 +0000

    add_tests

--- src/module_name/improve_accuracy.py
+++ src/module_name/improve_accuracy.py
@@ -11,13 +11,9 @@ codes = [x for x in dir(cv2) if x.startswith("COLOR_")]
 print(codes[:3])
 # ['COLOR_BAYER_BG2BGR', 'COLOR_BAYER_BG2BGRA', 'COLOR_BAYER_BG2BGR_EA']
 
-## print all color codes
-print(codes)
-image = cv2.imread("images/shell.png")
-print(image.shape)  # Returns (height, width, channels)
-img = cv2.imread("images/shell.png")
+img = cv2.imread("images/high-res.jpg")
 yuv = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
-cv2.imwrite("images/shell_b_w_yuv.png", yuv)
+cv2.imwrite("images/high_res_y_channel.png", yuv)
 
 y_channel = yuv[:, :, 0]
 
@@ -28,27 +24,3 @@ cv2.destroyAllWindows()
 
 # Optionally, you can save the Y channel as an image
 cv2.imwrite('y_channel.png', y_channel)
-
-# Read the TIFF image
-image = cv2.imread('images/20230215_SE2B_CGG_GBR_MS4_L3_BGRN.tif', cv2.IMREAD_UNCHANGED)
-
-# Check if the image was loaded successfully
-if image is not None:
-    resized_image = cv2.resize(image, (800, 600))
-    # Display the image
-    cv2.imshow('TIFF Image', resized_image)
-
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-else:
-    print("Failed to load the image")
-
-
-# Open your TIFF file
-dataset = gdal.Open('images/20230215_SE2B_CGG_GBR_MS4_L3_BGRN.tif')
-
-# Check if the file was opened successfully
-if dataset is None:
-    print("Failed to open the file.")
-else:
-    print(f"File opened successfully: {dataset.RasterXSize}x{dataset.RasterYSize}")
